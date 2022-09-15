8c64c4cc50ba998007dcd509e15ca15655f0baab
benjamindraper1996 <benjamin.draper@hotmail.co.uk>
2022-09-15 15:02:29 +0000

    overhaul theme.js to allow persistant theme

--- static/js/theme.js
+++ static/js/theme.js
@@ -2,9 +2,28 @@
  * The theme.js file controls the theme switch mode selector.
  */
 
-var themeSwitch = document.querySelector('input'); //Theme Switcher variable.
+function isLight() {
+  return localStorage.getItem("light-mode"); // Return storage of current of the theme.
+}
 
-//Listens for when someone clicks on the slider to change the theme.
-themeSwitch.addEventListener('change', function() {
-document.body.classList.toggle('dark-theme');
+function toggleRootClass() {
+  document.querySelector(":root").classList.toggle("light"); // Toggles current theme.
+}
+
+// Toggles current local storage variable.
+function toggleLocalStorageItem() {
+  if (isLight()) {
+    localStorage.removeItem("light-mode");
+  } else {
+    localStorage.setItem("light-mode", "set");
+  }
+}
+
+if (isLight()) {
+  toggleRootClass();
+}
+
+document.querySelector(".switch").addEventListener("click", () => {
+  toggleLocalStorageItem();
+  toggleRootClass();
 });
